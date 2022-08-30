79495f35f78a78bef968dc534621638ceea4fe01
adoc-github <65012009+adoc-github@users.noreply.github.com>
2022-08-30 14:37:52 +0900

    Remove rds_1

--- assets/rds_1/rds/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "hostname" {
-  value = "${aws_db_instance.default.endpoint}"
-}
-output "connection_string" {
-  sensitive = true
-  value = "Server=${aws_db_instance.default.address};Port=${aws_db_instance.default.port};Database=${var.db_name};Uid=${var.username};Pwd=${random_password.password.result};"
-}
