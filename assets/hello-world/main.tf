73a287360ef4a43c451fd8639ceb0f870cd58628
adoc-github <65012009+adoc-github@users.noreply.github.com>
2022-10-12 18:35:35 +0900

    Fix hello-world module

--- assets/hello-world/main.tf
+++ assets/hello-world/main.tf
@@ -1,8 +1,8 @@
 module "greeting" {
-  source = "../common/greeting"
+  source = "./common/greeting"
   my_name = var.my_name
 }
 
 module "response-url" {
-  source = "../common/response-url"
+  source = "./common/response-url"
 }
