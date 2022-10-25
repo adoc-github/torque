faa57785593997a14d5fb035905fd4ecd0303f94
adoc-github <65012009+adoc-github@users.noreply.github.com>
2022-10-25 14:15:31 +0900

    Fix ec2-instance grain.

--- assets/ec2-instance/main.tf
+++ assets/ec2-instance/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "testEC2" {
     ami = var.ami
     instance_type = var.instance_type
     subnet_id = "subnet-0124294163b6e76e0"
-    tags {
+    tags = {
         Name = "testEC2"
     }
 }
