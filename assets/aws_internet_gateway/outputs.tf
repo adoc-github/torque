29cb12dbf3900869d9d65c9b2b273d198fe99f32
adoc-github <65012009+adoc-github@users.noreply.github.com>
2022-11-30 10:24:28 +0900

    Add aws_internet_gateway tf module

---
+++ assets/aws_internet_gateway/outputs.tf
@@ -0,0 +1,9 @@
+output "igw_id" {
+  description = "The ID of the Internet Gateway"
+  value       = try(aws_internet_gateway.this[0].id, "")
+}
+
+output "igw_arn" {
+  description = "The ARN of the Internet Gateway"
+  value       = try(aws_internet_gateway.this[0].arn, "")
+}
---
+++ assets/aws_internet_gateway/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+    description = "Region where to create resources" 
+    type = string
+    default = "eu-west-1"
+}
+
+variable "create_igw" {
+  description = "Controls if an Internet Gateway is created for public subnets and the related routes that connect them."
+  type        = bool
+  default     = true
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+  default     = ""
+}
+
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "igw_tags" {
+  description = "Additional tags for the internet gateway"
+  type        = map(string)
+  default     = {}
+}
