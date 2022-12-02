cbb9177153784595d6a9ba79516ec7c70dc9e360
adoc-github <65012009+adoc-github@users.noreply.github.com>
2022-12-02 16:01:15 +0900

    Fix aws-subnet tf module

--- assets/aws-subnet/outputs.tf
+++ assets/aws-subnet/outputs.tf
@@ -1,19 +1,19 @@
 output "subnet_id" {
   description = "List of IDs of public subnets"
-  value       = aws_subnet.this[0].id
+  value       = aws_subnet.this[*].id
 }
 
 output "subnet_arn" {
   description = "List of ARNs of public subnets"
-  value       = aws_subnet.this[0].arn
+  value       = aws_subnet.this[*].arn
 }
 
 output "subnets_cidr_block" {
   description = "List of cidr_blocks of public subnets"
-  value       = compact(aws_subnet.this[0].cidr_block)
+  value       = compact(aws_subnet.this[*].cidr_block)
 }
 
 output "subnets_ipv6_cidr_block" {
   description = "List of IPv6 cidr_blocks of public subnets in an IPv6 enabled VPC"
-  value       = compact(aws_subnet.this[0].ipv6_cidr_block)
+  value       = compact(aws_subnet.this[*].ipv6_cidr_block)
 }
