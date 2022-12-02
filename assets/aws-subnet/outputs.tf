fd71fa39d0c31e16eb8655a1c8d9dc01f4b325b1
adoc-github <65012009+adoc-github@users.noreply.github.com>
2022-12-02 14:41:56 +0900

    Update aws-subnet tf module

--- assets/aws-subnet/outputs.tf
+++ assets/aws-subnet/outputs.tf
@@ -1,19 +1,19 @@
-output "subnets" {
+output "subnet_id" {
   description = "List of IDs of public subnets"
-  value       = aws_subnet.this[*].id
+  value       = aws_subnet.this[0].id
 }
 
-output "subnet_arns" {
+output "subnet_arn" {
   description = "List of ARNs of public subnets"
-  value       = aws_subnet.this[*].arn
+  value       = aws_subnet.this[0].arn
 }
 
-output "subnets_cidr_blocks" {
+output "subnets_cidr_block" {
   description = "List of cidr_blocks of public subnets"
-  value       = compact(aws_subnet.this[*].cidr_block)
+  value       = compact(aws_subnet.this[0].cidr_block)
 }
 
-output "subnets_ipv6_cidr_blocks" {
+output "subnets_ipv6_cidr_block" {
   description = "List of IPv6 cidr_blocks of public subnets in an IPv6 enabled VPC"
-  value       = compact(aws_subnet.this[*].ipv6_cidr_block)
+  value       = compact(aws_subnet.this[0].ipv6_cidr_block)
 }
