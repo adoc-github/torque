d8c041323dfb18a1967c7d3fbcfb0666aef3d794
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-01-31 17:04:36 +0900

    Update wordpress module

--- assets/wordpress/main.tf
+++ assets/wordpress/main.tf
@@ -2,7 +2,17 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.0.0/24"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0cd7ad8676931d727"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.example.id
 }
