7f81b7a80fa3dca2d8523d6e886252146087d949
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-02-07 16:17:45 +0900

    Fix wordpress module

--- assets/wordpress/main.tf
+++ assets/wordpress/main.tf
@@ -47,3 +47,14 @@ resource "aws_instance" "example" {
     sudo service apache2 restart
   EOF
 }
+
+resource "aws_route_table_association" "example" {
+  subnet_id = aws_subnet.example.id
+  route_table_id = aws_vpc.example.default_route_table_id
+}
+
+resource "aws_route" "example" {
+  route_table_id = aws_vpc.example.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.example.id
+}
