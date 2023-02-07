6339aa2094db1fdce07b682c81f6e311ae95d6a7
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-02-07 15:38:38 +0900

    Fix wordpress module

--- assets/wordpress/main.tf
+++ assets/wordpress/main.tf
@@ -6,9 +6,14 @@ resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/24"
 }
 
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
 resource "aws_subnet" "example" {
   vpc_id = aws_vpc.example.id
   cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_security_group" "example" {
@@ -29,17 +34,16 @@ resource "aws_instance" "example" {
   subnet_id = aws_subnet.example.id
   vpc_security_group_ids = [aws_security_group.example.id]
   key_name = "testadoc"
-  associate_public_ip_address = true
 
   user_data = <<-EOF
-      #!/bin/bash
-      sudo apt-get update
-      sudo apt-get install -y apache2
-      sudo apt-get install -y php libapache2-mod-php
-      sudo apt-get install -y mysql-server php-mysql
-      sudo apt-get install -y wordpress
-      sudo ln -s /usr/share/wordpress /var/www/html/wordpress
-      sudo chown -R www-data:www-data /var/www/html/wordpress
-      sudo service apache2 restart
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y apache2
+    sudo apt-get install -y php libapache2-mod-php
+    sudo apt-get install -y mysql-server php-mysql
+    sudo apt-get install -y wordpress
+    sudo ln -s /usr/share/wordpress /var/www/html/wordpress
+    sudo chown -R www-data:www-data /var/www/html/wordpress
+    sudo service apache2 restart
   EOF
 }
