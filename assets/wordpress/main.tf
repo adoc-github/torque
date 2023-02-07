8dbe59eff6aa17479eb4a65ebe15a0d3a8b29902
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-02-07 14:41:13 +0900

    Update wordpress module

--- assets/wordpress/main.tf
+++ assets/wordpress/main.tf
@@ -11,11 +11,25 @@ resource "aws_subnet" "example" {
   cidr_block = "10.0.0.0/24"
 }
 
+resource "aws_security_group" "example" {
+  name = "example"
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami = "ami-0cd7ad8676931d727"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.example.id
+  vpc_security_group_ids = [aws_security_group.example.id]
   key_name = "testadoc"
+  associate_public_ip_address = true
 
   user_data = <<-EOF
       #!/bin/bash
