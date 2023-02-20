d88b7289942a3a059fab07ce74676b1be49bed25
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-02-20 16:03:22 +0900

    Fix wordpress database grain

--- assets/wordpress-env/database/main.tf
+++ assets/wordpress-env/database/main.tf
@@ -65,6 +65,7 @@ resource "aws_db_instance" "example_db_instance" {
   allocated_storage     = 20
   storage_type          = "gp2"
   publicly_accessible   = false
+  skip_final_snapshot  = true
   db_subnet_group_name  = aws_db_subnet_group.example_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
 
