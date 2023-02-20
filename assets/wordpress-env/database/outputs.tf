b7fa36fa48546691125af4b754f2f1aa0661fec1
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-02-20 14:49:28 +0900

    Update wordpress database grain

--- assets/wordpress-env/database/outputs.tf
+++ assets/wordpress-env/database/outputs.tf
@@ -1,7 +1,7 @@
 output "hostname" {
-  value = "${aws_db_instance.default.endpoint}"
+  value = "${aws_db_instance.example_db_instance.endpoint}"
 }
 output "connection_string" {
   sensitive = true
-  value = "Server=${aws_db_instance.default.address};Port=${aws_db_instance.default.port};Database=${var.db_name};Uid=${var.username};Pwd=${random_password.password.result};"
+  value = "Server=${aws_db_instance.example_db_instance.address};Port=${aws_db_instance.example_db_instance.port};Database=${var.db_name};Uid=${var.username};Pwd=${random_password.password.result};"
 }
