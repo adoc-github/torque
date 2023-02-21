f944e6b7ea8fea0805ea0511ab966a173872c27d
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-02-21 16:23:58 +0900

    Update variables.tf

--- assets/wordpress-env/network/variables.tf
+++ assets/wordpress-env/network/variables.tf
@@ -2,15 +2,3 @@ variable "region" {
   description = "Region of RDS"
   default = "ap-northeast-1"
 }
-
-variable "db_name" {
-  description = "db name"
-}
-
-variable "username" {
-  description = "User name"
-}
-
-variable "password" {
-  description = "Password"
-}
