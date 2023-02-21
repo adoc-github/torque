30dab028af56e13d07ed946b0112640d0a8cce71
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-02-21 17:43:32 +0900

    Update main.tf

--- assets/wordpress-env/network/main.tf
+++ assets/wordpress-env/network/main.tf
@@ -38,6 +38,7 @@ resource "aws_subnet" "wordpress_subnet" {
   vpc_id     = aws_vpc.webapp_vpc.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "${var.region}d"
+  map_public_ip_on_launch = true
 }
 
 # セキュリティグループ
