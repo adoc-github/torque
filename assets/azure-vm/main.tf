dae5c7d8e5aa1ef80b322cb540e224540fd66d95
adoc-github <65012009+adoc-github@users.noreply.github.com>
2023-03-20 14:35:22 +0900

    Fix azure-vm grain

--- assets/azure-vm/main.tf
+++ assets/azure-vm/main.tf
@@ -52,12 +52,6 @@ resource "azurerm_linux_virtual_machine" "example" {
   network_interface_ids = [azurerm_network_interface.example.id]
   disable_password_authentication = false
 
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
